9e6be44b6522c92bc956f52a6e11821fbb5c3410
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-01-18 22:11:41 +0000

    Typescating samples

--- typeCasting.php
+++ typeCasting.php
@@ -1,2 +1,15 @@
 <?php
 //Typecasting in PHP
+# 1. Scalar Types
+        #Boolean
+        #int
+        #float
+        #string
+#  2. Compound Types
+        #array
+        #object
+        #callable
+        #Iterable
+#   3. Special Types
+        #resource
+        #null
