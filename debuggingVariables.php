a1c00ec213e804edf48cf8348f1ddabfeb36d376
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-01-18 21:08:44 +0000

    better way to debug using var_dump()

--- debuggingVariables.php
+++ debuggingVariables.php
@@ -3,4 +3,6 @@
 //We can use print_r to debug
 $students = ['mary', 'John', 'Grace', 'Philip'];
 echo $students,'<br>';//returns Array
-print_r($students);//list the arrays' values
+print_r($students,'<br>');//list the arrays' values
+//better way to debug using var_dump()
+var_dump($students);
