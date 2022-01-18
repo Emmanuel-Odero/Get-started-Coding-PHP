41dd6b3f0c71b715ddd965d96fdbc2032f210607
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-01-18 20:04:39 +0000

    PHP data types

---
+++ dataTypes.php
@@ -0,0 +1,13 @@
+<?php
+//Integers
+//Are none decimal numbers
+$num =132;
+$age = 345;
+//Boolean ---> are true or false statements
+$boolean =true; //couldas well be false
+//floats ---> are numbers with decimals
+$speed = 103.8;
+//arrays ---> are a colection of data store in a single variable
+$human = ["John", "28", 2.4,false];
+
+echo $boolean;//outputs one
