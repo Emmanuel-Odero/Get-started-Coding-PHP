e2a9720d17375f9ce30986c0eecff374415b7574
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-01-24 03:20:19 +0000

    array.php will fix the error later

---
+++ arrays.php
@@ -0,0 +1,30 @@
+<?php
+//Below is a simple array
+/*$names = array(
+    'Willis',
+    'Courtney',
+    'Paul',
+    'Marie'
+);
+$namesG = [
+    'Willis'=>22,
+    'Courtney'=>25,
+    'Paul'=>27,
+    'Marie'=>30
+];
+var_dump($namesG);
+echo "<br>" .$namesG['Willis']."<br>";*/
+//multidimensional Arrays
+$test = array(
+    'Kalonzo' => array(
+        'age'=>29,
+        'height'=>1.83,
+        'job'=>programmer,
+        'No. Friends'=>56,
+    ),
+    'Raila'=>26,
+    'Ruto'=>45,
+    'Musalia' => 70
+);
+var_dump($test);
+//echo "<br>Kalonzo is ".$arrays['Kalonzo']['Kalonzo']." years old";
