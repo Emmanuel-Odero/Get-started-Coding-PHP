93c884516447714b1f8716b781e55c4da01acd18
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-01-28 09:03:49 +0000

    started on discussions -discussions

---
+++ discussions/chat1-classes.php
@@ -0,0 +1,11 @@
+class male{
+    public $name;
+    public $gender;
+
+    function set_name(name){
+        echo $this -> name => $name;
+    }
+    function get_name(){
+        return $this -> name
+    }
+}
