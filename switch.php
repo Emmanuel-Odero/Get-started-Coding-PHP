8399c343658877d5c43853a64ec23deaeb07e807
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-01-29 19:47:00 +0000

    Switch statements

---
+++ switch.php
@@ -0,0 +1,20 @@
+<?php
+//switch statements are a faster way of witing if/else-if/else statements
+$color = $_GET["color"];
+switch ($color) {
+    case "red":
+    case "blue":
+    case "yellow":
+        //$color = $_Get['color']
+        break;
+    case "brown":
+        $color = "black";
+        break;
+    default:
+        $color = "green";
+        break;
+}
+?>
+<body style="background-color:<?php echo $color; ?>">
+The background color is <?php echo $color;?>
+</body>
