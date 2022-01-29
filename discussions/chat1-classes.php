8b6a701a6e77a05003c08e6cae5db96a8b79391b
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-01-29 17:55:55 +0000

    created class male

--- discussions/chat1-classes.php
+++ discussions/chat1-classes.php
@@ -1,11 +1,12 @@
+<?php
 class male{
     public $name;
     public $gender;
 
-    function set_name(name){
-        echo $this -> name => $name;
+    function set_name($name){
+        echo $this ->name = $name;
     }
     function get_name(){
-        return $this -> name
+        return $this -> name;
     }
 }
