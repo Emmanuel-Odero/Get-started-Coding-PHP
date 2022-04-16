e09ce5fffd3747eb7a4b75dbfbf29c2a2ec1df2a
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-04-16 08:34:25 +0300

    connected to db usind pdo

---
+++ modules/php-pdo-mysql-myadmin/quickwin.php
@@ -0,0 +1,18 @@
+<?php
+$db_host = 'localhost';
+$db_name = 'comapany';
+$db_username = 'root';
+$db_password = '';
+$dsn = "mysql:host=$db_host; dbname=$db_name";
+$db_connection = new PDO($dsn, $db_username, $db_password);
+//creating a querry
+$query = "SELECT * FROM users";
+$results=$db_connection->query($query);
+$db_connection = NULL;
+foreach($results as $result){
+    echo $result['name'];
+    echo $result['email'];
+    echo $result['country'];
+    echo $result['currency'];
+    echo "<br>";
+}
