4bc055d5a9d9ff0b5d429367483c15ef5f3b4ce1
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-04-16 15:06:33 +0300

    finished lesson querry from the db headed to inserting to DB

---
+++ modules/php-pdo-mysql-myadmin/live-site/www/query-from-db.php
@@ -0,0 +1,12 @@
+<?php
+require('../db_config.php');
+$query = "SELECT * FROM teachers";
+$results=$db_connection->query($query);
+$db_connection = NULL;
+foreach($results as $result){
+    echo $result['surName'], " ";
+    echo $result['email'], " ";
+    echo $result['startDate'], " ";
+    echo $result['currency'];
+    echo "<br>";
+}
