731ebe8aa4981711ca2aca17c004749d33e6c0bb
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-11-06 17:53:04 +0000

    done with the static site, yet to add php

---
+++ projects/madLibzGame/game.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome To MadLibz Game</title>
+</head>
+<div>
+    <div><h1>Hope you enjoy Playing Mad Libs Game</h1></div>
+    <div>
+        <h2>Please fill in the form Below</h2>
+    </div>
+    <br><br><br>
+    <div>
+        <form action="" method="post">
+            <div>
+                <label for="user-Name">Please Entre Your name: </label>
+                <input type="text" placeholder="Entre Your Name">
+            </div>
+            <br><br>
+            <div>
+                <label for="color">Please Entre Your Favorite Color: </label>
+                <input type="text" placeholder="Favorite Color">
+            </div>
+            <br><br>
+            <div>
+                <label for="artist">Please Entre Your Favorite Color: </label>
+                <input type="text" placeholder="Favorite Artist">
+            </div>
+            <br><br>
+        </form>
+    </div>
+    <br>
+    <div>
+        <h2>Your Text Has Been Generated Below: </h2>
+        <br>
+        <div>
+            <p>Hello <b>Mojo</b>, Welcome to mad libs. Roses are <b>yourColor</b>, you love so does <b>yourArtist</b></p>
+        </div>
+    </div>
+</div>
+
+<body>
+    
+</body>
+</html>
